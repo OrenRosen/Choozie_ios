7753585b7068546f5e9236153142899847ed8aa1
Oren Rosenblum <oren@ftbpro.com>
2014-07-19 20:03:02 +0300

    adding main choozie directory

---
+++ Choozie/Choozie/ChoozieViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChoozieViewController.h
+//  Choozie
+//
+//  Created by Oren Rosenblum on 19/07/2014.
+//  Copyright (c) 2014 ROKY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChoozieViewController : UIViewController
+
+@end
---
+++ Choozie/Choozie/ChoozieViewController.m
@@ -0,0 +1,29 @@
+//
+//  ChoozieViewController.m
+//  Choozie
+//
+//  Created by Oren Rosenblum on 19/07/2014.
+//  Copyright (c) 2014 ROKY. All rights reserved.
+//
+
+#import "ChoozieViewController.h"
+
+@interface ChoozieViewController ()
+
+@end
+
+@implementation ChoozieViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
