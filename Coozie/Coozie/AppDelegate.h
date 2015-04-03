24fac20c8c5be1cb4522898f69e73f69d86289ae
Oren Rosenblum <oren@90min.com>
2015-04-03 12:53:01 +0300

    add poroject

---
+++ Coozie/Coozie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coozie
+//
+//  Created by admin on 4/3/15.
+//  Copyright (c) 2015 Oren Rosenblum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
